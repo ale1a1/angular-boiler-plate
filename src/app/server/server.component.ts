e8ac6e96b48ffe0eb98ea7e04838cb8572e4d2fa
ale1a1 <68277684+ale1a1@users.noreply.github.com>
2022-12-04 09:45:36 +0000

    add directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
-
+  serverId: number = 10;
+  serverStatus: string = '';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    console.log(this.serverStatus);
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  getServerColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serversGroupName = '';
+  currentUserName = '';
+  isUserNameEmpty = true;
+  isServerCreated = false;
+  servers = [];
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  onCreateServer() {
+    this.isServerCreated = true;
+    this.servers.push('server');
+    console.log(this.servers);
+  }
+  // onUpdateServersGroupName(event: Event) {
+  //   this.serversGroupName = (<HTMLInputElement>event.target).value;
+  //   console.log(event.target);
+  // }
+  onUpdateUserName() {
+    if (this.currentUserName.length >= 1) this.isUserNameEmpty = false;
+  }
+
+  resetUserName() {
+    this.currentUserName = '';
+  }
 }
