ce0d9b0ac687bf736f5ca1ebc37248683575a55b
alessandro ladu <ale1a@libero.it>
2022-12-17 15:38:32 +0000

    create usersService

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  styleUrls: ['./inactive-users.component.css'],
 })
 export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+  users;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.users = this.usersService.inactiveUsers;
+  }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.usersService.setToActive(id);
   }
 }
